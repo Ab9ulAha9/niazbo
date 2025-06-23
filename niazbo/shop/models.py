e1632dcfd6e372561fa09a56ef3c36da1c9a8860
Ab9ulAha9 <ab9ulaha9@gmail.com>
2025-06-23 05:50:01 -0700

    till the discount

--- niazbo/shop/models.py
+++ niazbo/shop/models.py
@@ -12,16 +12,29 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+class discount (models.Model):
+    discount = models.IntegerField(default=0 , validators=[MinValueValidator(0)])
+    min_purchase=models.IntegerField(default=0 ,  validators=[MinValueValidator(0)] , help_text="enter the minimum  purchase at which discount starts ")
+    created_at=models.DateTimeField(auto_now=True)
 
 class Cart(models.Model):
     
     session_key = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
+    discount = models.IntegerField(default=0 , validators=[MinValueValidator(0)])
     def __str__(self):
         return f"Cart {self.id} - Session: {self.session_key}"
+    @property
     def total_price(self):
-        return sum(item.rotal_price for item in self.items.all())
+        return sum(item.total_price for item in self.items.all()) 
+    @property
+    def after_discount(self):
+        if self.discount >0:
+            return self.total_price-(self.total_price/100)*self.discount
+        return self.total_price
+        
+
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- niazbo/shop/views.py
+++ niazbo/shop/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render
-from .models import Product , Cart , CartItem
+from .models import Product , Cart , CartItem , discount 
 from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def get_cart (request):
-    if not (sess_key:= request.session.session_key):
+    if not ( request.session.session_key):
         request.session.create()
     sess_key=request.session.session_key
     cart,_=Cart.objects.get_or_create(session_key=sess_key)
+    d=discount.objects.first()
+    if cart.total_price>=d.min_purchase:
+        cart.discount=d.discount
+    else:
+        cart.discount=0
+    cart.save()
     return (cart)
 
+
+def update_item(request , value):
+    item_id=request.POST.get("item_id")
+    item=get_object_or_404(CartItem , id=item_id)
+    if value=="minus" :
+        if item.quantity >1:
+            item.quantity-=1
+    elif value=="add":
+        item.quantity+=1
+    item.save()
+    if value=="remove_form":
+        item.delete()
+    
+
+
 def cart (request ):
-    cart=get_cart(request)
-    return render("cart.html" , {"cart":cart}) 
+    if request.method=="POST":
+        value=request.POST.get("form")   
+        update_item(request , value )   
+    cart=get_cart(request)        
+    return render(request , "cart.html" , {"cart":cart}) 
+
 
 def create_item (request ):
     product_id=request.POST.get('product_id')
