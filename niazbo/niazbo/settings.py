25db1e7bfb378dd5c374747de1cb91091e195ace
Ab9ulAha9 <ab9ulaha9@gmail.com>
2025-08-09 17:27:08 -0700

    cloudinary

--- niazbo/niazbo/settings.py
+++ niazbo/niazbo/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
     'home' ,
     'shop',
     'orders',
@@ -154,7 +156,12 @@ STATIC_ROOT = BASE_DIR / "staticfiles"    # used in production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
